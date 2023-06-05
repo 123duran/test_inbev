e85e399cb39cca8b13ac692f69e3c98509016609
Bruno Duran <bgdbruno@gmail.com>
2023-06-05 17:00:05 -0300

    Adjusting logic.

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-def find_lowest_cost_vm(memory_gb, vcpu_cores, os, pricesheet_df, rightsizing_df):
+def find_lowest_cost_vm(memory_gb, vcpu_cores, pricesheet_df, rightsizing_df):
     filtered_df = rightsizing_df[(rightsizing_df['memoryInMB'] >= memory_gb * 1024) &
                                  (rightsizing_df['numberOfCores'] >= vcpu_cores) &
                                  (rightsizing_df['armSkuName'].isin(pricesheet_df['meterName'])) &
@@ -26,22 +25,22 @@ def main():
     print("Rightsizing:")
     rightsizing_df = pd.read_csv('Exam rightsizing.csv', delimiter=';', header=1)
     rightsizing_df.columns = ['armSkuName', 'numberOfCores', 'osDiskSizeInMB', 'memoryInMB', 'maxDataDiskCount', 'meterName']
-    vm1 = find_lowest_cost_vm(15, 4, 'Linux', pricesheet_df, rightsizing_df)
+    vm1 = find_lowest_cost_vm(15, 4, pricesheet_df, rightsizing_df)
     if vm1 is not None:
         print("VM 1 - Linux:")
         print(vm1)
 
-    vm2 = find_lowest_cost_vm(30, 2, 'Windows', pricesheet_df, rightsizing_df)
+    vm2 = find_lowest_cost_vm(30, 2, pricesheet_df, rightsizing_df)
     if vm2 is not None:
         print("VM 2 - Windows:")
         print(vm2)
 
-    vm3 = find_lowest_cost_vm(5, 1, 'Linux', pricesheet_df, rightsizing_df)
+    vm3 = find_lowest_cost_vm(5, 1, pricesheet_df, rightsizing_df)
     if vm3 is not None:
         print("VM 3 - Linux:")
         print(vm3)
 
-    vm4 = find_lowest_cost_vm(60, 6, 'Windows', pricesheet_df, rightsizing_df)
+    vm4 = find_lowest_cost_vm(60, 6, pricesheet_df, rightsizing_df)
     if vm4 is not None:
         print("VM 4 - Windows:")
         print(vm4)
